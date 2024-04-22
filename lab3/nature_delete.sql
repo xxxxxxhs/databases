680cd0f046bf478d90137df6b175683811731a0c
xxxxxx <148367988+xxxxxxhs@users.noreply.github.com>
2024-04-22 22:41:02 +0300

    Add files via upload

---
+++ lab3/nature_delete.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION remove_nature_objects()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM nature_object WHERE situated_at_area_id = OLD.id;
+    DELETE FROM enchantment WHERE source_area_obj_id = OLD.id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_remove_nature_objects
+BEFORE DELETE ON area_object
+FOR EACH ROW
+EXECUTE FUNCTION remove_nature_objects();
+
+
+-- Если удалется объект местности, то удаляются так же все объекты природы,
+-- которые с ним связаны
